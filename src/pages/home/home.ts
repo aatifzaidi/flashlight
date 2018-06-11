4ddeef6723340becb37d4cd028798d4b15a8218e
STI\aatifz <aatifsuntech@gmail.com>
2018-06-11 21:50:34 +0530

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Flashlight } from '@ionic-native/flashlight';
+import { Platform } from 'ionic-angular';
+
+// const flashlight = new Flashlight();
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  isOn: boolean =false;
+
+  constructor(private flashlight: Flashlight, private platform: Platform) {
+    this.platform.ready().then(() => {
+      this.updateFlashlightStatus();      
+    });
+  }
+
+  switchOn(){
+    this.flashlight.switchOn();
+    this.updateFlashlightStatus();
+  }
+
+  switchOff(){
+    this.flashlight.switchOff();
+    this.updateFlashlightStatus();
+  }
+
+  toggle(){
+    this.flashlight.toggle();
+    this.updateFlashlightStatus();
+  }
+
+  updateFlashlightStatus() {
+    this.isOn = this.flashlight.isSwitchedOn();
+  }
+}
